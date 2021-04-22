07260b47a00f8057d51390b87c231fc3efb510de
Rsaliyev Madiyar <mrsaliyev@gmail.com>
2021-04-23 01:16:38 +0600

    done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { PartIComponent } from './part-i/part-i.component';
+import { PartIiComponent } from './part-ii/part-ii.component';
+import { AppRoutingModule } from './app-routing.module';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PartIComponent,
+    PartIiComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    ReactiveFormsModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
